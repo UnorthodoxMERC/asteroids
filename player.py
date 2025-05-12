5f0b70353d80ea686fad68f1bc773122b7beeaa1
UnorthodoxMERC <kboomer224@aol.com>
2025-05-12 10:56:54 -0500

    finished up to ch 3 ls 3 of asteroids game build

---
+++ player.py
@@ -0,0 +1,59 @@
+import pygame
+from constants import *
+from circleshape import CircleShape
+
+
+class Player(CircleShape):
+    def __init__(self, x, y):
+        super().__init__(x, y, PLAYER_RADIUS)
+        self.rotation = 0
+
+    def draw(self, screen):
+        pygame.draw.polygon(screen, "white", self.triangle(), 2)
+
+    def triangle(self):
+        forward = pygame.Vector2(0, 1).rotate(self.rotation)
+        right = pygame.Vector2(0, 1).rotate(self.rotation + 90) * self.radius / 1.5
+        a = self.position + forward * self.radius
+        b = self.position - forward * self.radius - right
+        c = self.position - forward * self.radius + right
+        return [a, b, c]
+
+    def rotate(self, dt):
+        self.rotation += PLAYER_TURN_SPEED * dt
+    
+    def update(self, dt):
+        keys = pygame.key.get_pressed()
+
+        if keys[pygame.K_a]:
+            self.rotate(-dt)
+        if keys[pygame.K_d]:
+            self.rotate(dt)
+        if keys[pygame.K_w]:
+            self.move(dt)
+        if keys[pygame.K_s]:
+            self.move(-dt)
+        if keys[pygame.K_SPACE]:
+            self.shoot()
+
+    def move(self, dt):
+        forward = pygame.Vector2(0, 1).rotate(self.rotation)
+        self.position += forward * PLAYER_SPEED * dt
+
+    def shoot(self):
+        shot_pos = self.position
+        direction = pygame.Vector2(0, 1)
+        rotated = direction.rotate(self.angle)
+        velocity = rotated * PLAYER_SHOOT_SPEED
+
+
+class Shot(CircleShape):
+    def __init__(self, x, y, radius, velocity):
+            super().__init__(x, y, radius)
+            self.velocity = velocity
+        
+    def draw(self, screen):
+            pygame.draw.circle(screen, "white", self.position, self.radius, 2) 
+
+    def update(self, dt):
+            self.position += (self.velocity * dt)
